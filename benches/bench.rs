83ad1c49b90e411f16889264b3e280d115b6b793
jmfiaschi <jm.fiaschi@gmail.com>
2021-12-16 20:21:47 +0100

    fix(bench): typo

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,27 +3,27 @@ use json_value_resolve::Resolve;
 use serde_json::Value;
 
 fn merge(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Resolve");
+    let mut group = c.benchmark_group("resolve");
 
-    group.bench_function("Bench Value::resolve(scalar_null).", |b| {
+    group.bench_function("scalar_null", |b| {
         b.iter(|| black_box(Value::resolve("null".to_string())))
     });
-    group.bench_function("Bench Value::resolve(scalar_unsigned).", |b| {
+    group.bench_function("scalar_unsigned", |b| {
         b.iter(|| black_box(Value::resolve("10".to_string())))
     });
-    group.bench_function("Bench Value::resolve(scalar_signed).", |b| {
+    group.bench_function("scalar_signed", |b| {
         b.iter(|| black_box(Value::resolve("-00010000".to_string())))
     });
-    group.bench_function("Bench Value::resolve(scalar_float).", |b| {
+    group.bench_function("scalar_float", |b| {
         b.iter(|| black_box(Value::resolve("10.50".to_string())))
     });
-    group.bench_function("Bench Value::resolve(scalar_float_signed).", |b| {
+    group.bench_function("scalar_float_signed", |b| {
         b.iter(|| black_box(Value::resolve("-10.50".to_string())))
     });
-    group.bench_function("Bench Value::resolve(scalar_bool).", |b| {
+    group.bench_function("scalar_bool", |b| {
         b.iter(|| black_box(Value::resolve("true".to_string())))
     });
-    group.bench_function("Bench Value::resolve(scalar_char).", |b| {
+    group.bench_function("scalar_char", |b| {
         b.iter(|| black_box(Value::resolve("my string".to_string())))
     });
     group.finish();
